8447b76499fc37c8c8921c20af5a82f4bfc15c4f
KafuiMawunyo <siabikafui123@gmail.com>
2024-07-17 11:39:41 +0000

    adding all files

---
+++ src/login.js
@@ -0,0 +1,9 @@
+//dropdown for join food safari
+
+function dropdownLabel() {
+    let label = document.getElementById("label");
+    label.style.display = "block";
+  }
+  
+  let clickb = document.getElementById("clickb");
+  clickb.addEventListener("click", dropdownLabel);
--- src/product.js
+++ src/product.js
@@ -17,4 +17,44 @@ function dropdownWatermelon() {
   orangeH.addEventListener("click", dropdownOrange);
 
 //dropdown for rice
+
+function dropdownRice() {
+    let rice = document.getElementById("rice");
+    rice.style.display = "block";
+  }
+  
+  let riceH = document.getElementById("riceH");
+  riceH.addEventListener("click", dropdownRice);
+
+  //dropdown for maize
+
+function dropdownMaize() {
+  let maize = document.getElementById("maize");
+  maize.style.display = "block";
+}
+
+let maizeH = document.getElementById("maizeH");
+maizeH.addEventListener("click", dropdownMaize);
+
+//dropdown for ketchup
+
+function dropdownKetchup() {
+  let ketchup = document.getElementById("ketchup");
+  ketchup.style.display = "block";
+}
+
+let ketchupH = document.getElementById("ketchupH");
+ketchupH.addEventListener("click", dropdownKetchup);
+
+//dropdown for cocoyam
+
+function dropdownCocoyam() {
+  let cocoyam = document.getElementById("cocoyam");
+  cocoyam.style.display = "block";
+}
+
+let cocoyamH = document.getElementById("cocoyamH");
+cocoyamH.addEventListener("click", dropdownCocoyam);
+
+
   
